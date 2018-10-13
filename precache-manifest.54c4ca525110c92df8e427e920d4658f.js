23dd3b50446db1451447cd47f078688b52840503
Ksenia <k.lanetskaya@gmail.com>
2018-10-13 21:56:33 +0300

    Update build

---
+++ precache-manifest.54c4ca525110c92df8e427e920d4658f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "239deafa2bcab64574752642b0515246",
+    "url": "/react-calc/static/media/bg.239deafa.png"
+  },
+  {
+    "revision": "ea893f69b44ce11eeb0a42eb0e2113d4",
+    "url": "/react-calc/static/media/HelveticaNeueCyr-Light.ea893f69.woff"
+  },
+  {
+    "revision": "a35e8116882e5d5e5be9c2a1556cb3c3",
+    "url": "/react-calc/static/media/HelveticaNeueCyr-Light.a35e8116.woff2"
+  },
+  {
+    "revision": "a26eac4a4e99110e9fe74bb213f753b0",
+    "url": "/react-calc/static/media/HelveticaNeueCyr-Light.a26eac4a.eot"
+  },
+  {
+    "revision": "71b2279e43ea80a51999e2a53c3b49c7",
+    "url": "/react-calc/static/media/HelveticaNeueCyr-Light.71b2279e.svg"
+  },
+  {
+    "revision": "364ef4b3b164907ef7ba",
+    "url": "/react-calc/static/js/runtime~main.364ef4b3.js"
+  },
+  {
+    "revision": "982948590326b60ba297",
+    "url": "/react-calc/static/js/main.98294859.chunk.js"
+  },
+  {
+    "revision": "a336fb588946dec7dc3f",
+    "url": "/react-calc/static/js/1.a336fb58.chunk.js"
+  },
+  {
+    "revision": "8f862d89cbc325f8629bb245d53de909",
+    "url": "/react-calc/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b929cc77bb61e15da7f4a90aeba5dac9.js"
+  "/react-calc/precache-manifest.54c4ca525110c92df8e427e920d4658f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-calc/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
