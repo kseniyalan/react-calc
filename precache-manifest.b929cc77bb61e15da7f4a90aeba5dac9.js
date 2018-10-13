51a065febf4f8b8095a9761edd7a0365dbc97e2a
Ksenia <k.lanetskaya@gmail.com>
2018-10-13 21:45:05 +0300

    Add the build

---
+++ precache-manifest.b929cc77bb61e15da7f4a90aeba5dac9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "239deafa2bcab64574752642b0515246",
+    "url": "/static/media/bg.239deafa.png"
+  },
+  {
+    "revision": "ea893f69b44ce11eeb0a42eb0e2113d4",
+    "url": "/static/media/HelveticaNeueCyr-Light.ea893f69.woff"
+  },
+  {
+    "revision": "a35e8116882e5d5e5be9c2a1556cb3c3",
+    "url": "/static/media/HelveticaNeueCyr-Light.a35e8116.woff2"
+  },
+  {
+    "revision": "a26eac4a4e99110e9fe74bb213f753b0",
+    "url": "/static/media/HelveticaNeueCyr-Light.a26eac4a.eot"
+  },
+  {
+    "revision": "71b2279e43ea80a51999e2a53c3b49c7",
+    "url": "/static/media/HelveticaNeueCyr-Light.71b2279e.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc1068bab65fcb2cd8e2",
+    "url": "/static/js/main.cc1068ba.chunk.js"
+  },
+  {
+    "revision": "a336fb588946dec7dc3f",
+    "url": "/static/js/1.a336fb58.chunk.js"
+  },
+  {
+    "revision": "bbd466f44c2db857ccfd76060cd692bd",
+    "url": "/index.html"
+  }
+];
